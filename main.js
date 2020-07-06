d83d975bec10664e711c4ee11d83e437fdf927cf
teros1121 <56314823+teros1121@users.noreply.github.com>
2020-07-06 13:45:30 -0700

    Calculate the values

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
-function add (){
-
+//A fucntion which uses the text field 'p1' and grabs the value of that number
+//The function then returns the percentage (of 1000)
+//Example: p1 value is 99
+//This function should return 990
+//Example: 25, return 250
+function percent (COST) {
+var per = Number(document.getElementById("p1").value);
+var per = per * .01;
+return per * COST;
 }
